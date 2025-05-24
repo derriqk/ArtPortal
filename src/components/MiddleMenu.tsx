16762ef71f67d931b67d394b346bfee2f87cbb94
jkind889 <jova.kind@bison.howard.edu>
2025-05-24 13:47:30 -1000

    fix to image
    fixed middle menu images

--- src/components/MiddleMenu.tsx
+++ src/components/MiddleMenu.tsx
@@ -1,10 +1,11 @@
 import { Container } from 'react-bootstrap';
+import Image from 'next/image';
 
 const MiddleMenu = () => (
   <Container id="middle-menu" className="py-3">
     <div className="image-text-wrapper">
       <div className="left-side">
-        <img id="left-images" src="/images/aespa.png" alt="Aespa" />
+        <Image id="left-images" src="/images/aespa.png" alt="Aespa" />
         <div className="below-image-text">
           <p>Testing123</p>
         </div>
