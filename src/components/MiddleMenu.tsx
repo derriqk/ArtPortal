c5c4c34780bca1fd368490187060fc580a1f8eab
jkind889 <jova.kind@bison.howard.edu>
2025-05-24 16:18:06 -1000

    Formatting Middle Menu
    - fixed the image format finally
    - added featured art to homepage
    - started formatting the middle meny

--- src/components/MiddleMenu.tsx
+++ src/components/MiddleMenu.tsx
@@ -1,17 +1,30 @@
-import { Container } from 'react-bootstrap';
-import Image from 'next/image';
+import { Container, Image } from 'react-bootstrap';
 
 const MiddleMenu = () => (
-  <Container id="middle-menu" className="py-3">
+  <Container id="middle-menu" className="py-3" fluid>
     <div className="image-text-wrapper">
       <div className="left-side">
+        <strong> Featured Art </strong>
+        <Image id="left-images" src="/images/aespa.png" alt="Aespa" />
+        <div className="below-image-text">
+          <p>Poster design for Aespa's "WHILPASH" album</p>
+        </div>
+        <Image id="left-images" src="/images/aespa.png" alt="Aespa" />
+        <div className="below-image-text">
+          <p>Testing123</p>
+        </div>
         <Image id="left-images" src="/images/aespa.png" alt="Aespa" />
         <div className="below-image-text">
           <p>Testing123</p>
         </div>
       </div>
+      <div className="vl" />
+
       <div className="right-side">
-        <strong>Testing</strong>
+        <div className="right-info-box">
+          <strong> Testing </strong>
+          <p> details</p>
+        </div>
       </div>
     </div>
   </Container>
