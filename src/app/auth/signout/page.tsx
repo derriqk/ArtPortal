0676cc2b9be27450c0b172802a118b377f56d6b3
Derrick Quiamas <quiamasderrick@gmail.com>
2025-06-03 14:24:43 -1000

    fixed the sizing

--- src/app/auth/signout/page.tsx
+++ src/app/auth/signout/page.tsx
@@ -1,27 +1,33 @@
 'use client';
 
 import { signOut } from 'next-auth/react';
-import { Button, Col, Row } from 'react-bootstrap';
+import { Button, Card, Col, Container, Row } from 'react-bootstrap';
 
 /** After the user clicks the "SignOut" link in the NavBar, log them out and display this page. */
 const SignOut = () => (
-  <Col id="signout-page" className="text-center py-3">
-    <h2>Do you want to sign out?</h2>
-    <Row>
-      <Col xs={4} />
-      <Col>
-        <Button variant="danger" onClick={() => signOut({ callbackUrl: '/', redirect: true })}>
-          Sign Out
-        </Button>
-      </Col>
-      <Col>
-        <Button variant="secondary" href="/">
-          Cancel
-        </Button>
-      </Col>
-      <Col xs={4} />
+  <Container className="text-center py-5 signfont">
+    <Row className="align-items-center justify-content-center">
+      <Card className="signcard py-2">
+        <Card.Title className="pt-4">
+          Do you want to sign out?
+        </Card.Title>
+        <Card.Body>
+          <Row>
+            <Col>
+              <Button className="signbutton" onClick={() => signOut({ callbackUrl: '/', redirect: true })}>
+                Sign Out
+              </Button>
+            </Col>
+            <Col>
+              <Button variant="secondary" href="/">
+                Cancel
+              </Button>
+            </Col>
+          </Row>
+        </Card.Body>
+      </Card>
     </Row>
-  </Col>
+  </Container>
 );
 
 export default SignOut;
