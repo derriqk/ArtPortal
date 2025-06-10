7a203a3fe5737141976ba7bb9a538dd7403aad0e
jkind889 <jova.kind@bison.howard.edu>
2025-06-09 21:14:38 -1000

    fix

--- src/app/about/page.tsx
+++ src/app/about/page.tsx
@@ -1,4 +1,4 @@
-import { Col, Container, Image, Row } from 'react-bootstrap';
+import { Col, Container, Row } from 'react-bootstrap';
 
 const aboutme = () => (
   <Container className="text-start mt-5" id="aboutme fluid">
