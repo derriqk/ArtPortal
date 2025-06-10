0653f701a036115895cde74a879ed76445e46472
jkind889 <jova.kind@bison.howard.edu>
2025-06-09 15:13:28 -1000

    about us

--- src/app/about/page.tsx
+++ src/app/about/page.tsx
@@ -1,8 +1,22 @@
 import { Col, Container, Image, Row } from 'react-bootstrap';
 
 const aboutme = () => (
-  <Container className="text-start mt-5" id="aboutme">
-    <p> bored </p>
+  <Container className="text-start mt-5" id="aboutme fluid">
+    <h1 className="head"> - About Us - </h1>
+    <p className="small-text">
+      Lorem ipsum dolor sit amet,
+      consectetur adipiscing elit,
+      sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.
+      Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.
+      Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur.
+      xcepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
+    </p>
+    <div className="big-text">
+      <p>Designs</p>
+      <p>by</p>
+      <p>YBGCORP</p>
+    </div>
+
   </Container>
 );
 
