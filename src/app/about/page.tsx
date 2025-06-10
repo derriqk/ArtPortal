6afd7afc30d6e51432ac53cb7e6d15ced8fb7a27
jkind889 <jova.kind@bison.howard.edu>
2025-06-09 16:30:05 -1000

    about me 2

--- src/app/about/page.tsx
+++ src/app/about/page.tsx
@@ -11,11 +11,36 @@ const aboutme = () => (
       Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur.
       xcepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
     </p>
-    <div className="big-text">
-      <p>Designs</p>
-      <p>by</p>
-      <p>YBGCORP</p>
-    </div>
+    <Row className="align-items-center">
+      <Col xs={3} className="big-text">
+        <p>Designs</p>
+        <p>by</p>
+        <p>YBGCORP</p>
+      </Col>
+      <Col xs={9} className="left-text">
+        <h2>Who we are </h2>
+        <p className="small-text">
+          Lorem ipsum dolor sit amet,
+          consectetur adipiscing elit,
+          sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.
+          Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.
+          Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur.
+          xcepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
+        </p>
+      </Col>
+      <Row className="">
+        <Col xs={3} className="">
+          <p className="small-text">
+            Lorem ipsum dolor sit amet,
+            consectetur adipiscing elit,
+            sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.
+            Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.
+            Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur.
+            xcepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum
+          </p>
+        </Col>
+      </Row>
+    </Row>
 
   </Container>
 );
